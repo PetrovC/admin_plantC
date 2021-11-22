6dc1158cff8c1b55f2256705e9ae83a67de168de
zakaria264 <92975095+zakaria264@users.noreply.github.com>
2021-11-22 16:03:46 +0100

    Routing
    route

---
+++ src/Pages/Inscriptions.js
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material";
+import React from 'react';
+
+
+const Inscription= ()=> {
+    
+
+        
+
+
+        return (
+            
+
+            <main>
+                
+                <Typography component ='h1' variant='h3'>
+                    Agenda
+                </Typography>
+
+            </main>
+        )
+
+    
+
+}
+export default Inscription;
